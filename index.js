5207df53f4fce802c47c1e1e1a2bfd554e5d5a34
Thomas Watson Steen <w@tson.dk>
2014-05-03 22:03:59 +0700

    First version

---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var util = require('util');
+var https = require('https');
+var os = require('os');
+var EventEmitter = require('events').EventEmitter;
+
+var httpsOptions = {
+  hostname: 'api.opsgenie.com',
+  path: '/v1/json/customer/heartbeat',
+  method: 'POST'
+};
+
+var OpsGenie = function () {};
+util.inherits(OpsGenie, EventEmitter);
+
+OpsGenie.prototype.heartbeat      = start;
+OpsGenie.prototype._error         = error;
+OpsGenie.prototype._config        = config;
+OpsGenie.prototype._sendHeartbeat = sendHeartbeat;
+
+var opsgenie = module.exports = exports = new OpsGenie();
+
+// auto-configure using environment variables
+process.nextTick(function () {
+  if (opsgenie._configuration) return;
+  opsgenie.heartbeat();
+});
+
+function start(options) {
+  opsgenie._config(options);
+  opsgenie._sendHeartbeat();
+  setInterval(opsgenie._sendHeartbeat, 5000 * 60);
+}
+
+function config(options) {
+  if (!options) options = {};
+  opsgenie._configuration = {
+    apiKey: options.apiKey || process.env.OPSGENIE_API_KEY,
+    source: options.source || process.env.OPSGENIE_SOURCE || os.hostname()
+  };
+}
+
+function error(err) {
+  if (opsgenie.listeners('error').length)
+    opsgenie.emit('error', err);
+}
+
+function sendHeartbeat() {
+  var req = https.request(httpsOptions, function (res) {
+    var data = '';
+
+    res.setEncoding('utf8');
+
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function () {
+      var json = {};
+
+      if (data) {
+        try {
+          json = JSON.parse(data);
+        } catch (err) {
+          error(err);
+          return;
+        }
+      }
+
+      if (res.statusCode !== 200 || json.code !== 200)
+        error(new Error('Unexpected OpsGenie response (code: ' + res.statusCode + ', msg: ' + json.status + ')'));
+
+      if (data)
+        opsgenie.emit('heartbeat', json);
+    });
+  });
+
+  req.on('error', error);
+
+  req.end(JSON.stringify({
+    apiKey: opsgenie._configuration.apiKey,
+    source: opsgenie._configuration.source
+  }));
+}
---
+++ test.js
@@ -0,0 +1,188 @@
+'use strict';
+
+var assert = require('assert');
+var os = require('os');
+var sinon = require('sinon');
+var nock = require('nock');
+var opsgenie = require('./index');
+
+nock.disableNetConnect();
+
+assert('heartbeat' in opsgenie, 'Expected opsgenie to expose a heartbeat function');
+assert('_config' in opsgenie, 'Expected opsgenie to expose a _config function');
+assert('_error' in opsgenie, 'Expected opsgenie to expose an _error function');
+assert('_sendHeartbeat' in opsgenie, 'Expected opsgenie to expose a _sendHeartbeat function');
+assert(!('_configuration' in opsgenie), 'Opsgenie should wait to configure it self until after the first tick');
+
+var testConf = function () {
+  var conf = { apiKey: 'api-key', source: 'source' };
+  opsgenie._config(conf);
+  return conf;
+};
+
+var testResponse = function () {
+  return {
+    heartbeat: 1,
+    status: 'successful',
+    code: 200
+  };
+};
+
+sinon.stub(opsgenie, 'heartbeat'); // we have to stup this before next tick
+describe('OpsGenie object', function () {
+  after(function () {
+    opsgenie.heartbeat.restore();
+  });
+
+  it('should auto-start', function () {
+    sinon.assert.calledOnce(opsgenie.heartbeat);
+  });
+});
+
+describe('._config()', function () {
+  var origConf;
+
+  beforeEach(function () {
+    origConf = opsgenie._configuration;
+  });
+
+  afterEach(function () {
+    opsgenie._configuration = origConf;
+  });
+
+  it('should set the _configuration object', function () {
+    var conf = { apiKey: 1, source: 1 };
+    opsgenie._config(conf);
+    assert.deepEqual(opsgenie._configuration, conf);
+  });
+
+  it('should fall back to environment variables', function () {
+    process.env.OPSGENIE_API_KEY = 'env_api_key';
+    process.env.OPSGENIE_SOURCE = 'env_source';
+
+    var conf = { apiKey: process.env.OPSGENIE_API_KEY, source: process.env.OPSGENIE_SOURCE };
+
+    opsgenie._config();
+    assert.deepEqual(opsgenie._configuration, conf);
+
+    delete process.env.OPSGENIE_API_KEY;
+    delete process.env.OPSGENIE_SOURCE;
+  });
+
+  it('should fall back to os hostname', function () {
+    opsgenie._config();
+    assert.equal(opsgenie._configuration.source, os.hostname());
+  });
+});
+
+describe('._error()', function () {
+  it('should not emit if nobody is listening', function () {
+    opsgenie._error(new Error()); // would throw if it didn't work
+  });
+
+  it('should emit if a listener is added', function () {
+    var err = new Error();
+    var spy = sinon.spy();
+    opsgenie.on('error', spy);
+    opsgenie._error(err);
+    sinon.assert.calledOnce(spy);
+    sinon.assert.calledWith(spy, err);
+    opsgenie.removeListener('error', spy);
+  });
+});
+
+describe('.heartbeat()', function () {
+  var clock;
+
+  beforeEach(function () {
+    clock = sinon.useFakeTimers('setInterval');
+    sinon.stub(opsgenie, '_sendHeartbeat');
+    opsgenie.heartbeat();
+  });
+
+  afterEach(function () {
+    clock.restore();
+    opsgenie._sendHeartbeat.restore();
+  });
+
+  it('should send a heartbeat immediately', function () {
+    sinon.assert.calledOnce(opsgenie._sendHeartbeat);
+  });
+
+  it('should send a heartbeat again after 5 minutes', function () {
+    clock.tick(5000 * 60);
+    sinon.assert.calledTwice(opsgenie._sendHeartbeat);
+  });
+});
+
+describe('._sendHeartbeat()', function () {
+  var api;
+
+  describe('everything ok', function () {
+    beforeEach(function () {
+      api = nock('https://api.opsgenie.com')
+        .post('/v1/json/customer/heartbeat', testConf())
+        .reply(200, {
+          heartbeat: 1,
+          status: 'successful',
+          code: 200
+        });
+    });
+
+    afterEach(function () {
+      nock.cleanAll();
+    });
+
+    it('should send a heartbeat to OpsGenie', function (done) {
+      setTimeout(function () {
+        api.done();
+        done();
+      }, 25);
+      opsgenie._sendHeartbeat();
+    });
+
+    it('should emit a heartbeat event', function (done) {
+      opsgenie.once('heartbeat', function (res) {
+        assert.deepEqual(res, testResponse());
+        done();
+      });
+      opsgenie._sendHeartbeat();
+    });
+  });
+
+  describe('error handling', function () {
+    afterEach(function () {
+      nock.cleanAll();
+    });
+
+    it('should emit a error event if the response have a bad status code', function (done) {
+      nock('https://api.opsgenie.com')
+        .post('/v1/json/customer/heartbeat', testConf())
+        .reply(500);
+
+      opsgenie.once('error', function (err) {
+        assert(err instanceof Error);
+        done();
+      });
+
+      opsgenie._sendHeartbeat();
+    });
+
+    it('should emit a error event if the response json isn\'t as expected', function (done) {
+      nock('https://api.opsgenie.com')
+        .post('/v1/json/customer/heartbeat', testConf())
+        .reply(200, {
+          heartbeat: 1,
+          status: 'successful',
+          code: 500
+        });
+
+      opsgenie.once('error', function (err) {
+        assert(err instanceof Error);
+        done();
+      });
+
+      opsgenie._sendHeartbeat();
+    });
+  });
+});
